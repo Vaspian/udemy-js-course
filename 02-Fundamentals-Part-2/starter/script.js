b43244da143938ea7d1e034f0b5bf4cb6c10b1ad
Vaspian <112447754+Vaspian@users.noreply.github.com>
2024-10-26 16:54:13 -0500

    Update script.js

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/* let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+// const interface = "Audio";
+// const private = 534; */
+
+// FUNCTIONS
+
+function logger() {
+  console.log("My name is Sebastian");
+}
+
+// Calling / running / invoking the function
+
+logger();
+logger();
+logger();
