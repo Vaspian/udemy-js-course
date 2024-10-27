9d4fc8a39437f31b7a51b101ff1e77cda84de7a1
Vaspian <112447754+Vaspian@users.noreply.github.com>
2024-10-26 19:36:08 -0500

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -17,6 +17,18 @@ function logger() {
 
 // Calling / running / invoking the function
 
-logger();
-logger();
-logger();
+// logger();
+// logger();
+// logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
