89340eee88374e5a0acc1bcde2b919e67d68a54f
Sebastian Rosales <112447754+Vaspian@users.noreply.github.com>
2025-01-15 21:29:29 +0000

    Test

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -412,3 +412,5 @@ const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 console.log(
   `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
 );
+
+console.log(`The most amazing!😊`);
